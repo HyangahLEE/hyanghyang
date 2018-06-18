26ea5303f5135b4969029d1cce29ed180686ae26
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-06-18 12:47:39 +0900

    Add files via upload

---
+++ Ex02.java
@@ -0,0 +1,28 @@
+package days01;
+
+// 자바 프로그램의 기본 구조
+// 1. 반드시 하나의 시작개체로 사용될 클래스 선언
+// 2. main() 함수 선언   <- java.exe
+public class Ex02 {	
+	public static void main(String[] args) {
+		
+		// 콘솔창에 본인이름 또는  "hello world~" 문자열 출력 코딩
+		// 집에서 액자 벽에 걸려고 한다. 
+		// 벽에 못치는 일(기능) 하려고 합니다. 
+		// 물건( 망치 ) == 객체 == 개체 == Object
+		
+		// [S]ystem 클래스
+		// 망치의 못치는일(기능)
+		// 망치의 못빼는일(기능)
+		//System.in  표준 입력 부분
+		//System.out  표준 출력 부분
+		
+		// 망치.못치는부분.망치질하다(일)  함수,메서드
+		System.out.println("hello world~");
+		System.out.println("이창익");
+		
+		// Ctrl + A  전체 선택
+		// Ctrl + i   자동 들여쓰기
+		// 이클리스에서  자주 사용되는 단축키...
+	}
+}
