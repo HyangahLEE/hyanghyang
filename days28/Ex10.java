335fa3661fbd2ee98b74824e6c78ec6cf69366a5
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 16:39:44 +0900

    Update Ex10.java

--- days28/Ex10.java
+++ days28/Ex10.java
@@ -1,52 +1,57 @@
 package days28;
 
 class WorkThread extends Thread{
-	public  WorkThread (ThreadGroup threadGroup, String threadName) {
-	super(threadGroup, threadName);
-
-}
-	
+	//  생성자.
+	public WorkThread(ThreadGroup tg, String tname) {
+		super(tg, tname); // 스레드그룹, 스레드명
+	}
 	@Override
 	public void run() {
 		while(true) {
-			
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				System.out.println(getName() + "interrupted");
-			break;
+				System.out.println(this.getName()+" interrupted.");
+				break;
 			}
-			
-			
 		}
-		System.out.println(getName() + "종료 !!");
-		
+		System.out.println(this.getName() +" 종료됨.");
 	}
-	
-	
-	
-	
 }
 
 public class Ex10 {
 
 	public static void main(String[] args) {
-		ThreadGroup tg =  new ThreadGroup("tg");
+		// p 624 스레드 그룹 생성, 정보 출력
+		//    스레드 그룹 안의 모든 스레드 interrupt() 메소드 호출-> 일괄종료.
 		
-		WorkThread a= new WorkThread(tg, "a");
-		WorkThread b = new WorkThread(tg, "b");
+		ThreadGroup myGroup = new ThreadGroup("myGroup");
 		
-		a.start();
-		b.start();
-		System.out.println( "[ main 스레드 그룹의  list() 메소드 출력 내용 ]");
-		ThreadGroup mg = Thread.currentThread().getThreadGroup();
-		mg.list();
+		WorkThread ta = new WorkThread(myGroup, "ta");
+		WorkThread tb = new WorkThread(myGroup, "tb");
+		
+		ta.start();
+		tb.start();
+		
+		System.out.println("[ main 스레드 그룹의 list() 메소드 출력 내용 ]");
+		
+		// main 스레드그룹
+		ThreadGroup mainGroup =
+				Thread.currentThread().getThreadGroup();
+		
+		mainGroup.list();
+		
 		System.out.println();
 		
 		try {
 			Thread.sleep(3000);
-		} catch (InterruptedException e) {}
-		mg.interrupt();
+		} catch (InterruptedException e) { 
+			e.printStackTrace();
+		}
+		
+		System.out.println("[ myGroup 스레드 그룹의 interrupt() 메소드 호출]");
+		
+		myGroup.interrupt(); // 모든 스레드가 interrupt() 메소드 처리...
 	}
 
 }
