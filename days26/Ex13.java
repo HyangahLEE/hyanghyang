90f1d9accc640a8ca9fdcf050c4ca010f08c5f64
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 16:57:19 +0900

    Update Ex13.java

--- days26/Ex13.java
+++ days26/Ex13.java
@@ -2,21 +2,20 @@ package days26;
 
 public class Ex13 {
 
-	public static void main(String[] args) {
-		// Java 스레드 사용하는 방법
-		// 1. Runnable 인터페이스    구현
-		// 2. Thread    클래스          상속
-		
-		// main 스레드  -  main() 메소드 독립적으로 수행.
-		/*
-		Thread t = Thread.currentThread();
-		String t_name = t.getName();
-		*/
-		
-		String t_name = Thread.currentThread().getName();
-		System.out.println("> 현재 main() 메소드를 실행하는 스레드명  : " 
-		                         + t_name );
-
-	}
-
+public static void main(String[] args) {
+	//java 스레드 사용하는 방법
+	// 1. Runnable 인터페이스 구현(implement)
+	// 2. Thread  클래스    상속 (extends)
+	
+	// main 스레드 - main() 메소드 독립적으로 수행
+	/*
+	Thread t = Thread.currentThread(); // 현재 독립적인 스레드.(일꾼)
+	t.getName(); //스레드의 이름반환 메서드호출.
+	String t_name = t.getName();
+	*/
+	String t_name = Thread.currentThread().getName();
+	System.out.println("> 현재 main() 메소드를 실행하는 스레드명 :" + t_name );
+	
+	
+}
 }
