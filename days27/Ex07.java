ad45d067c8ced2e8ba89103fa26c68e7bbd3fdf7
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 16:47:00 +0900

    Update Ex07.java

--- days27/Ex07.java
+++ days27/Ex07.java
@@ -1,45 +1,43 @@
 package days27;
 
 class 노가다Worker extends Thread{
-	
 	@Override
 	public void run() {
-	//	for (int i = 1; i <=100; i++) {
-			System.out.println("> 삽질..");
+		//for (int i = 0; i < 100; i++) {
+			System.out.println("> 삽질....");
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(100);  // 0.1초간 notRunnable 상태가됨.   (다른 스레드한테 cpu한테 넘기기 위해서) !! 슬립사용..
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}	
-	//}
-}
+		}
+		
+	}
+//}
 
 public class Ex07 {
 
 	public static void main(String[] args) {
-
-		노가다Worker hyangah = new 노가다Worker();
-		
-		System.out.println("1"+hyangah.getState());
-		hyangah.start();// Start -> Runnable
-		System.out.println("2"+hyangah.getState());
-		
+		// 스레드의 상태와 제어
+		노가다Worker  hyang = new 노가다Worker();
+		//getStater() 메소드 : [NEW] 상태.
+		System.out.println( "1."+hyang.getState() );
+		hyang.start(); // 러너블 상태 ..실행직전까지..
+		System.out.println( "2."+hyang.getState() );
 		
 		try {
-			Thread.sleep(100);
-			System.out.println("3"+hyangah.getState());
-		} catch (InterruptedException e) {
+			Thread.sleep(100); // 메인스레드  Start -- >  cpu가 not러너블상태로..쉬러감( *초간)..일안함.  ->  //hyang이 런상태가됨. 
+			System.out.println( "3."+hyang.getState() );   // TIMED_WAITING
+		} catch (InterruptedException e) { // 강제로 
 			e.printStackTrace();
 		}
-		
 		
 		try {
 			Thread.sleep(5000);
-			System.out.println("4"+hyangah.getState());
+			System.out.println( "4."+hyang.getState() );
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		
 	}
-
 }
