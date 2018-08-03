350034a14959f26c7b75edf5b7120d7669e2ac24
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 17:26:14 +0900

    Update Ex06.java

--- days02/Ex06.java
+++ days02/Ex06.java
@@ -8,24 +8,30 @@ public class Ex06 {
 		long l = 100000000000L;
 		System.out.println(l);
 	
+		// byte 0 = 0000 0000
+ 		//   int 0 = 0000 0000 0000 0000 0000 0000 0000 0000
 		// 0   =  4byte
 		//0L   = long형 0
 		
 		
-		
-		short  s ;
+	// 스택영역 2byte 만큼 메모리 할당 -32768 ~ 32767 정수를 저장할 수 있는 s라는 이름을 가진 변수 선언
+	short  s ;
 	s = 100;
-	
+		
+		
+	// 스택영역에 -21억 ~ 21억 정수를 저장하는 변수 i 선언
 	/*int i ; 
 	i = 10000;
 	System.out.println(i);
 
 	
 	*/
-/*	
+/*	// 8byte
 	long l = 10;
 	System.out.println(l);
 	
+	//Duplicate local variable l
+		// 같은 지역에 변수가 중복선언 되어 있다.
 	l = 1000;
 	System.out.println(l);
 	
