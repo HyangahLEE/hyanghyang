a934beb37372a8c0196bf7eee30e9114f1101ea2
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 17:12:33 +0900

    Update Ex17.java

--- days24/Ex17.java
+++ days24/Ex17.java
@@ -21,12 +21,9 @@ public class Ex17 {
 		byte[] b = new byte[100]; // 버퍼
 		int readByteNo;
 		String pathname = ".\\src\\days24\\BBS_201707141109490060.jpg";
-		//이미지 크기?
-				//파일의 대한 정보 중 이미지크기 ? XXX byte
-				File imgFile = new File(pathname);
-				long fileLength = imgFile.length();
-				// format  ( 데이터, 심플데이터, 데시몰포멧 )
-				//System.out.printf(" > 이미지 파일 : %d bytes\n" , fileLength );
+	
+		File imgFile = new File(pathname); //File 객체 
+				
 		
 		try {
 			//           is스트림           XXXInputStream(File)
