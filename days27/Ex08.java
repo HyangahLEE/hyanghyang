9a4278b8cc6a17bf103fb97a2b13ff40473223a3
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 16:47:54 +0900

    Update Ex08.java

--- days27/Ex08.java
+++ days27/Ex08.java
@@ -1,32 +1,40 @@
 package days27;
 
-class CalcThread extends Thread{
-	public CalcThread(String name ) {
-		setName(name);
-	}
-	
+class PriorityThread extends Thread{
 	public void run() {
-		for (int i = 0; i <2000000000; i++) {
-			
+		int i =0;
+		System.out.printf(">> %s [우선권:%d] 시작\n"
+				, this.getName(), this.getPriority());
+		while(i<10000) {
+			i++;
+			try {
+				this.sleep(1);
+			} catch (InterruptedException e) { 
+				e.printStackTrace();
+			}
 		}
-		System.out.println(getName());
+		System.out.printf("<< %s [우선권:%d] 종료\n"
+				, this.getName(), this.getPriority());
 	}
-	
 }
 
 public class Ex08 {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <=10; i++) {
-			Thread thread = new CalcThread("thread" + i);
-			if(i !=10) {
-				thread.setPriority(Thread.MIN_PRIORITY);
-			}else {
-				thread.setPriority(Thread.MAX_PRIORITY);
-			}
-						thread.start();
-		}
+		//
+		System.out.println("> main 스레드 시작 ");
 
+		PriorityThread [] p = new PriorityThread[10];
+		for (int i = 0; i < 10; i++) {
+			p[i] = new PriorityThread();
+			p[i].setName("t"+(i+1));
+			p[i].setPriority(i+1);
+		}
+		for (int i = 0; i < p.length; i++) {
+			p[i].start();
+		}
+		
+		System.out.println("> main 스레드 종료 ");
 	}
 
 }
