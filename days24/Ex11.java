457ddc5de2f6822872ebea6400c666e0bbdd2259
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 17:15:15 +0900

    Update Ex11.java

--- days24/Ex11.java
+++ days24/Ex11.java
@@ -21,7 +21,7 @@ public class Ex11 {
 		// vs [s][s][][][
 		// vr []
 		// Type mismatch: cannot convert 
-		//                 from Vector<SalesMan> to Vector<Regular>
+		//                 from Vector<SalesMan> to Vector<Regular>  //형변환 못함.
 		/*
 		Vector<SalesMan> vs = new Vector<>();
 		Vector<Regular>    vr = vs ; 
@@ -32,7 +32,7 @@ public class Ex11 {
 		Vector<SalesMan> vs = new Vector<>();
 		//Vector<Regular> vs = new Vector<>();
 		//Vector<Temp> vs = new Vector<>();
-		Vector<? extends Employee>    vr = vs ; 
+		Vector<? extends Employee>    vr = vs ; //Employee 상속 받은거 vs에 담겟다..
 		
 		// Vector<? extends Number> vt = ??;
 		
