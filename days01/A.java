f49a0f1101ab6a28275bde7bfa4dd2c1aa2ceb38
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-08-03 17:20:53 +0900

    Update A.java

--- days01/A.java
+++ days01/A.java
@@ -1,18 +1,23 @@
 package days01;
 
-public class A {
-
+public class A {    //상속클래스 확인.day16 ex01 4,5
+	
+	
+	// 부모클래스의 인자가 있는 생성자를 명시적으로 선언.
+	public A(int pub) {
+		this.pub = pub;
+		System.out.println(" > A  1 생성자 호출됨.");
+	}
+	//디폴트 생성자도 명시적으로 선언해 주어야 한다.
+	public A() {
+		System.out.println(" > A 디폴트 생성자 호출됨.");
+	}
+	//fields
 	private int pri;
 	protected int pro;
 	int def;
 	public int pub;
 	
-	public A() {
-		System.out.println("> A 디폴트 생성자 호출됨");
-	}
-
-	public A(int pub) {
-	this.pub= pub;
 	
-	}
+
 }
