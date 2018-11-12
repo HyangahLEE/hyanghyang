9d5c006d72d2ba5f6363c8f2cb5977b201bd3baf
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-11-12 16:24:43 +0900

    Update Ex05.java

--- days18/Ex05.java
+++ days18/Ex05.java
@@ -1,23 +1,30 @@
 package days18;
 
+import java.io.IOException;
+
+import org.omg.CORBA.Request;
+
 import days17.Regular;
 
 public class Ex05 {
 
-	public static void main(String[] args)  {
-		//Regular emp = new Regular();
-		
+	public static void main(String[] args) {
+		// Regular emp = new Regular();
+		// 
 		String className = "days17.Regular";
-		//Unhandled exception type ClassNotFoundException
-		//forName() : 클래스의 객체를 리턴
+		// Unhandled exception type ClassNotFoundException
+		// forName() : 클래스의 객체를 리턴하는 메소드
 		try {
-			Class.forName(className);
+			// p 493 동적 객체 생성
+			Class<Regular> clazz = (Class<Regular>) Class.forName(className);// Regular emp;
+			//Regular emp =  clazz.newInstance();
 		} catch (ClassNotFoundException e) {
-			
 			e.printStackTrace();
-		}
+		} 
 		
-		
+		// Unhandled exception type IOException
+		//System.in.read();
+
 	}
 
 }
