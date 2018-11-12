72907ff74684d007558c7029dc897ee462c80e90
HyangahLEE <40186915+HyangahLEE@users.noreply.github.com>
2018-11-12 16:26:33 +0900

    Update Ex07.java

--- days18/Ex07.java
+++ days18/Ex07.java
@@ -1,10 +1,30 @@
 package days18;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Ex07 {
 
 	public static void main(String[] args) {
-		String name;
+		String name = null;
+		try {
+			name = inputData();
+		} catch (IOException e) { 
+			e.printStackTrace();
+		}
+		System.out.println(name);
+	}
 
+	private static String inputData() throws IOException {
+		//Scanner scanner = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in)
+                                    );
+		System.out.println("> 이름 입력 ? ");
+		String name =  br.readLine();
+		return name;
 	}
 
 }
